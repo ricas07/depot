85cbffd52a198d72dd21cedae9fa153c9157cdab
Ricardo Perez <ricas07@gmail.com>
2012-10-10 17:53:11 -0700

    added cart and line items with relationships

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  def time
-    @current_time = Time.now.asctime
-  end
+  private
+    def current_cart
+      Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      cart
+    end
+
 end
